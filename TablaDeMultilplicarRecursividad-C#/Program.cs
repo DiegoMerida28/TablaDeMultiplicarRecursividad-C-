94dcadb2bdd901676a32c292e52cc245e304b606
DiegoMerida28 <145516421+DiegoMerida28@users.noreply.github.com>
2023-10-28 21:04:02 -0600

    Agregar archivos de proyecto.

---
+++ TablaDeMultilplicarRecursividad-C#/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void TablaMultiplicarRecursiva(int numero, int multiplicador, int limite)
+    {
+        if (multiplicador > limite)
+        {
+            return;
+        }
+
+        Console.WriteLine($"{numero} x {multiplicador} = {numero * multiplicador}");
+
+        TablaMultiplicarRecursiva(numero, multiplicador + 1, limite);
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese el número para la tabla de multiplicar: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el límite para la tabla de multiplicar: ");
+        int limite = int.Parse(Console.ReadLine());
+
+        TablaMultiplicarRecursiva(numero, 1, limite);
+    }
+}
